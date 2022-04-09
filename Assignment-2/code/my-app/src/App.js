b0d8f1c1eb0312662da0e672b863ba44d12814b3
khalid915146 <khalidanwar915146@gmail.com>
2022-04-10 00:25:01 +0530

    Aassignment_4_submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,28 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
 
 function App(props) {
- const {employee}=props;
-  const{name,location,bloodGroup,age,profileImg}=props.employee
-return(
-  <div className="container">
-  {/* Start your React code here */}
-  <div className='Img'>
-    <img src={profileImg}/> 
-  </div>
-  <h1>{name}</h1>
-  <p>Location</p>
-  <h2>{location}</h2>
-  <p>Blood group</p>
-  <h2>{bloodGroup}</h2>
-  <p>Age</p>
-  <h2>{age}</h2>
-</div>
-)
-}
+  const { employee } = props;
+  // const { name, location, bloodGroup, age, profileImg } = props.employee
+  console.log(employee)
+  return (
+    <div >
+      {/* Start your React code here */}
+      {employee.map(emp => {
+        return (
+          <div className="container">
+            <div className='Img'>
+              <img src={emp.profileImg} alt='img' />
+            </div>
+            <h1>{emp.name}</h1>
+            <p>Location</p>
+            <h2>{emp.location}</h2>
+            <p>Blood group</p>
+            <h2>{emp.bloodGroup}</h2>
+            <p>Age</p>
+            <h2>{emp.age}</h2>
 
-
-
+          </div>
+        )
+      })}
+    </div>)}
 export default App;
